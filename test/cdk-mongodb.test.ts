07eb84fb8ef3e9d3f2d661ea45ff70f482e3d967
Seitarou Miura <seimiura@amazon.com>
2024-10-22 18:38:48 +0900

    Initial commit

---
+++ test/cdk-mongodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMongodb from '../lib/cdk-mongodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-mongodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMongodb.CdkMongodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
